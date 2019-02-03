dfd5f3aeed4fcc46d14fd25ad9c3dbd81745e4b1
Verbőczy Kristóf <verbocika@gmail.com>
2019-02-03 17:28:59 +0100

    Constructor tests

--- GameTest.cpp
+++ GameTest.cpp
@@ -1,20 +1,27 @@
 #include "Game.h"
 #include <gtest/gtest.h>
+#include <string>
 
-
-TEST(IsPlayableTest, NotPlayable)
+TEST (GameConstructorTest, CreateQuestions)
 {
-	Game testGame;
-	testGame.add("Chet");
-	ASSERT_EQ(false, testGame.isPlayable());
-	testGame.add("Chat");
-	ASSERT_EQ(true, testGame.isPlayable());
-	//ASSERT_EQ(false, false);
+  Game testGame;
+  for (int i = 0; i < 50; i++)
+  {
+    ASSERT_EQ("Pop Question " + std::to_string(i), testGame.getPopQuestion(i));
+    ASSERT_EQ("Science Question " + std::to_string(i), testGame.getScienceQuestion(i));
+    ASSERT_EQ("Sports Question " + std::to_string(i), testGame.getSportsQuestion(i));
+    ASSERT_EQ("Rock Question " + std::to_string(i), testGame.getRockQuestion(i));
+  }
 }
 
+TEST (GameConstructorTest, CurrentPlayer)
+{
+  Game testGame;
+  ASSERT_EQ(0, testGame.getCurrentPlayer());
+}
 
 int main(int argc, char **argv)
 {
-	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
